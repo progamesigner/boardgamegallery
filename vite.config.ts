6ba371f7094529021d4cfe80cea15b7d266faa0d
Yang Sheng Han <progamesigner@gmail.com>
2023-02-25 06:47:04 +0000

    Make static build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import type { UserConfigExport } from 'vite'
 
+import { default as adapter } from 'solid-start-static'
 import { default as solid } from 'solid-start/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     solid({
-      ssr: false,
+      adapter: adapter(),
     }),
   ],
-}) satisfies UserConfigExport
+}) as UserConfigExport
