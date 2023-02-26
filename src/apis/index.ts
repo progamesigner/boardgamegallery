c4b61c23cca077ca8cb292a455be7372173965d4
Yang Sheng Han <progamesigner@gmail.com>
2023-02-26 16:27:54 +0000

    Allow more flexible parameters for stores

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,11 +3,17 @@ import type { Game } from '~/types'
 import { default as notion } from './notion'
 
 export async function fetchGames(source: string): Promise<Array<Game>> {
-  const [engine, params] = source.split(':', 2)
-  switch (engine.toLowerCase()) {
-    case 'notion':
-      return await notion(params)
-    default:
-      throw new Error('Unsupported source engine')
+  const params = source.split(':')
+  const store = params.shift()
+  if (store) {
+    switch (store.toLowerCase()) {
+      case 'notion': {
+        const [databaseId] = params
+        return await notion(databaseId)
+      }
+      default:
+        throw new Error('Unsupported data store')
+    }
   }
+  throw new Error('No data store specified')
 }
