abdf476d00b5aab1d56fff58e3f07fe9df7ec93e
Yang Sheng Han <progamesigner@gmail.com>
2023-03-02 16:10:08 +0000

    Update game item styles

--- src/components/GameDetail.tsx
+++ src/components/GameDetail.tsx
@@ -2,12 +2,45 @@ import type { JSX } from 'solid-js'
 
 import type { GameObject } from '~/types'
 
-import { Index } from 'solid-js'
+import { Index, Match, Show, Switch } from 'solid-js'
 
 import { GameImage } from '~/components/GameImage'
 
 interface Props {
   item: GameObject
+}
+
+interface RangePanelProps {
+  format: (range: string) => string
+  maximum: number
+  minimum: number
+}
+
+interface TagsPanelProps {
+  tags: Array<string>
+}
+
+function TagsPanel(props: TagsPanelProps): JSX.Element {
+  return (
+    <Show when={props.tags.length > 0}>
+      <Index each={props.tags}>{tag => <span class="text-gray-300">{tag()}</span>}</Index>
+    </Show>
+  )
+}
+
+function RangePanel(props: RangePanelProps): JSX.Element {
+  return (
+    <Show when={props.maximum > 0 && props.minimum > 0 && props.maximum >= props.minimum}>
+      <Switch>
+        <Match when={props.maximum === props.minimum}>
+          <span>{props.format(`${props.minimum}`)}</span>
+        </Match>
+        <Match when={props.maximum > props.minimum}>
+          <span>{props.format(`${props.minimum} ~ ${props.maximum}`)}</span>
+        </Match>
+      </Switch>
+    </Show>
+  )
 }
 
 export function GameDetail(props: Props): JSX.Element {
@@ -19,25 +52,30 @@ export function GameDetail(props: Props): JSX.Element {
         </figure>
       </div>
       <div class="mx-4">
-        <h3 class="text-base text-gray-300">{props.item.originalName}</h3>
-        <h2 class="text-lg font-bold text-gray-100">{props.item.name}</h2>
+        <h2 class="overflow-hidden overflow-ellipsis whitespace-nowrap text-xl font-bold text-gray-100">
+          {props.item.name}
+        </h2>
+        <h3 class="overflow-hidden overflow-ellipsis whitespace-nowrap font-bold text-gray-300">{props.item.originalName}</h3>
         <p class="text-gray-300">{props.item.description}</p>
         <p>
-          Type:
-          <Index each={props.item.types}>
-            {item => <span class="text-gray-300">{item()}</span>}
-          </Index>
+          Type: <TagsPanel tags={props.item.types} />
         </p>
         <p>
-          Players: {props.item.minimalPlayers} ~ {props.item.maximalPlayers}
+          <RangePanel
+            format={range => `Player: ${range} players`}
+            minimum={props.item.minimalPlayers}
+            maximum={props.item.maximalPlayers}
+          />
         </p>
         <p>
-          Time: {props.item.minimalMinutes} ~ {props.item.maximalMinutes} mins
+          <RangePanel
+            format={range => `Time: ${range} minutes`}
+            minimum={props.item.minimalMinutes}
+            maximum={props.item.maximalMinutes}
+          />
         </p>
         <div>
-          <Index each={props.item.tags}>
-            {item => <span class="text-gray-300">{item()}</span>}
-          </Index>
+          <TagsPanel tags={props.item.tags} />
         </div>
       </div>
     </div>
