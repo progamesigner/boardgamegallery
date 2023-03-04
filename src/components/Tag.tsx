1d7d2c312f7a19327b72e14a5eb27bc76dc796ee
Yang Sheng Han <progamesigner@gmail.com>
2023-03-04 08:13:07 +0000

    Fix tag styles

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -24,45 +24,45 @@ export function Tag(props: Props): JSX.Element {
   return (
     <Switch>
       <Match when={props.color === TagColor.AMBER}>
-        <span class="badge mr-2 rounded bg-amber-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-amber-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.BLUE}>
-        <span class="badge mr-2 rounded bg-blue-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-blue-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.CYAN}>
-        <span class="badge mr-2 rounded bg-cyan-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-cyan-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.GREEN}>
-        <span class="badge mr-2 rounded bg-green-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-green-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.INDIGO}>
-        <span class="badge mr-2 rounded bg-indigo-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-indigo-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.ORANGE}>
-        <span class="badge mr-2 rounded bg-orange-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-orange-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.PINK}>
-        <span class="badge mr-2 rounded bg-pink-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-pink-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.PURPLE}>
-        <span class="badge mr-2 rounded bg-purple-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-purple-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.RED}>
-        <span class="badge mr-2 rounded bg-red-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-red-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.TEAL}>
-        <span class="badge mr-2 rounded bg-teal-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-teal-700 py-2">{props.children}</span>
       </Match>
       <Match when={props.color === TagColor.YELLOW}>
-        <span class="badge mr-2 rounded bg-yellow-700 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-yellow-700 py-2">{props.children}</span>
       </Match>
       <Match when={true}>
-        <span class="badge mr-2 rounded bg-gray-300 py-2 last:mr-0">{props.children}</span>
+        <span class="badge rounded bg-gray-300 py-2">{props.children}</span>
       </Match>
     </Switch>
   )
 }
 
 export function Tags(props: ParentProps): JSX.Element {
-  return <div>{props.children}</div>
+  return <div class="flex gap-2 justify-start flex-wrap">{props.children}</div>
 }
