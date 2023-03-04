826f74316a0602d56cfadea05e0e29bcced5b237
Yang Sheng Han <progamesigner@gmail.com>
2023-03-04 08:10:26 +0000

    Update to use tag component

--- src/components/GameDetail.tsx
+++ src/components/GameDetail.tsx
@@ -5,6 +5,7 @@ import type { GameObject } from '~/types'
 import { Index, Match, Show, Switch } from 'solid-js'
 
 import { GameImage } from '~/components/GameImage'
+import { Tag, Tags } from '~/components/Tag'
 
 interface Props {
   item: GameObject
@@ -15,20 +16,6 @@ interface RangePanelProps {
   format: (range: string) => string
   maximum: number
   minimum: number
-}
-
-interface TagsPanelProps {
-  tags: Array<string>
-}
-
-function TagPanel(props: TagsPanelProps): JSX.Element {
-  return (
-    <div>
-      <Show when={props.tags.length > 0}>
-        <Index each={props.tags}>{tag => <span class="text-gray-300">{tag()}</span>}</Index>
-      </Show>
-    </div>
-  )
 }
 
 function RangePanel(props: RangePanelProps): JSX.Element {
@@ -53,14 +40,14 @@ function RangePanel(props: RangePanelProps): JSX.Element {
 
 export function GameDetail(props: Props): JSX.Element {
   return (
-    <div class="max-h-screen sm:max-h-96 w-full overflow-y-auto px-4 pb-4 text-gray-100">
+    <div class="max-h-screen w-full overflow-y-auto px-4 pb-4 text-gray-100 sm:max-h-96">
       <h2 class="overflow-hidden overflow-ellipsis whitespace-nowrap text-xl font-bold">
         {props.item.name}
       </h2>
       <h3 class="overflow-hidden overflow-ellipsis whitespace-nowrap text-gray-300">
         {props.item.originalName}
       </h3>
-      <div class="flex flex-row gap-4 pt-2 pb-4">
+      <div class="flex flex-row items-start gap-4 pt-2 pb-4">
         <div class="relative shrink-0 basis-5/12">
           <figure class="pb-[125%]">
             <GameImage item={props.item} />
@@ -68,7 +55,9 @@ export function GameDetail(props: Props): JSX.Element {
         </div>
         <div class="flex flex-col">
           <div>
-            <TagPanel tags={props.item.types} />
+            <Tags>
+              <Index each={props.item.types}>{type => <Tag>{type()}</Tag>}</Index>
+            </Tags>
             <RangePanel
               fallback={() => '玩家人數: 未知'}
               format={range => `玩家人數: ${range} 人`}
@@ -86,7 +75,9 @@ export function GameDetail(props: Props): JSX.Element {
             <p class="mt-2 border-t border-gray-500 pt-2">{props.item.description}</p>
           </Show>
           <div class="mt-auto">
-            <TagPanel tags={props.item.tags} />
+            <Tags>
+              <Index each={props.item.tags}>{tag => <Tag>{tag()}</Tag>}</Index>
+            </Tags>
           </div>
         </div>
       </div>
