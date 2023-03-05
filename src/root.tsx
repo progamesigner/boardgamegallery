11ff735901daa99ebebc237b5629dc686e7591c6
Yang Sheng Han <progamesigner@gmail.com>
2023-03-05 06:24:25 +0000

    Move components

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'solid-js'
+import { Suspense } from 'solid-js'
 import {
   Body,
   ErrorBoundary,
@@ -13,16 +13,6 @@ import {
 
 import './root.css'
 
-const Header = lazy(async () => {
-  const module = await import('~/components/Header')
-  return { default: module.Header }
-})
-
-const Footer = lazy(async () => {
-  const module = await import('~/components/Footer')
-  return { default: module.Footer }
-})
-
 export default function Root() {
   return (
     <Html lang="en">
@@ -31,39 +21,14 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body class="flex h-screen flex-col">
-        <header class="mb-2">
-          <div class="container mx-auto">
-            <Suspense>
-              <ErrorBoundary>
-                <Header />
-              </ErrorBoundary>
-            </Suspense>
-          </div>
-        </header>
-
-        <main class="mb-2">
-          <div class="container mx-auto">
-            <Suspense>
-              <ErrorBoundary>
-                <Routes>
-                  <FileRoutes />
-                </Routes>
-              </ErrorBoundary>
-            </Suspense>
-          </div>
-        </main>
-
-        <footer class="footer footer-center mt-auto bg-base-300 p-4">
-          <div class="container mx-auto">
-            <Suspense>
-              <ErrorBoundary>
-                <Footer />
-              </ErrorBoundary>
-            </Suspense>
-          </div>
-        </footer>
-
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
         <Scripts />
       </Body>
     </Html>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,16 @@ import { createSignal, Index, Match, onMount, Show, Switch } from 'solid-js'
 import { Portal } from 'solid-js/web'
 import { useSearchParams } from 'solid-start'
 
-import { fetchGames } from '~/apis'
 import { ErrorMessage } from '~/components/ErrorMessage'
+import { Footer } from '~/components/Footer'
 import { GameDetail } from '~/components/GameDetail'
-import { ImageCacheProvider } from '~/components/GameImage'
 import { GameItem } from '~/components/GameItem'
+import { Header } from '~/components/Header'
+import { ImageCacheProvider } from '~/components/GameImage'
 import { Loading } from '~/components/Loading'
 import { Modal, ModalClose, ModalTrigger } from '~/components/Modal'
+
+import { fetchGames } from '~/apis'
 
 export default function (): JSX.Element {
   const [searchParams] = useSearchParams()
@@ -48,55 +51,73 @@ export default function (): JSX.Element {
   })
 
   return (
-    <div class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
-      <Switch fallback={<Loading />}>
-        <Match when={!getSource()}>
-          <ErrorMessage>No source available ...</ErrorMessage>
-        </Match>
-        <Match when={getLoading()}>
-          <Loading />
-        </Match>
-        <Match when={getError()}>
-          <ErrorMessage>Error!</ErrorMessage>
-        </Match>
-        <Match when={!getLoading() && !getError()}>
-          <ImageCacheProvider>
-            <Index each={getGames()} fallback={<ErrorMessage>No Data ...</ErrorMessage>}>
-              {item => (
-                <>
-                  <ModalTrigger id={`modal-${item().id}`}>
-                    <GameItem item={item()} />
-                  </ModalTrigger>
-                  <Portal>
-                    <Modal id={`modal-${item().id}`}>
-                      <ModalClose id={`modal-${item().id}`}>
-                        <Show when={item().bggId}>
-                          <a
-                            href={`https://boardgamegeek.com/boardgame/${item().bggId}`}
-                            class="flex flex-row items-center text-sm text-gray-100"
-                          >
-                            BGG
-                            <span class="ml-1 h-4 w-4 fill-none stroke-current stroke-1">
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                                />
-                              </svg>
-                            </span>
-                          </a>
-                        </Show>
-                      </ModalClose>
-                      <GameDetail item={item()} />
-                    </Modal>
-                  </Portal>
-                </>
-              )}
-            </Index>
-          </ImageCacheProvider>
-        </Match>
-      </Switch>
+    <div class="flex h-screen flex-col">
+      <header class="mb-2">
+        <div class="container mx-auto">
+          <Header />
+        </div>
+      </header>
+
+      <main class="mb-2">
+        <div class="container mx-auto">
+          <div class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+            <Switch fallback={<Loading />}>
+              <Match when={!getSource()}>
+                <ErrorMessage>No source available ...</ErrorMessage>
+              </Match>
+              <Match when={getLoading()}>
+                <Loading />
+              </Match>
+              <Match when={getError()}>
+                <ErrorMessage>Error!</ErrorMessage>
+              </Match>
+              <Match when={!getLoading() && !getError()}>
+                <ImageCacheProvider>
+                  <Index each={getGames()} fallback={<ErrorMessage>No Data ...</ErrorMessage>}>
+                    {item => (
+                      <>
+                        <ModalTrigger id={`modal-${item().id}`}>
+                          <GameItem item={item()} />
+                        </ModalTrigger>
+                        <Portal>
+                          <Modal id={`modal-${item().id}`}>
+                            <ModalClose id={`modal-${item().id}`}>
+                              <Show when={item().bggId}>
+                                <a
+                                  href={`https://boardgamegeek.com/boardgame/${item().bggId}`}
+                                  class="flex flex-row items-center text-sm text-gray-100"
+                                >
+                                  BGG
+                                  <span class="ml-1 h-4 w-4 fill-none stroke-current stroke-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                      <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                                      />
+                                    </svg>
+                                  </span>
+                                </a>
+                              </Show>
+                            </ModalClose>
+                            <GameDetail item={item()} />
+                          </Modal>
+                        </Portal>
+                      </>
+                    )}
+                  </Index>
+                </ImageCacheProvider>
+              </Match>
+            </Switch>
+          </div>
+        </div>
+      </main>
+
+      <footer class="footer footer-center mt-auto bg-base-300 p-4">
+        <div class="container mx-auto">
+          <Footer />
+        </div>
+      </footer>
     </div>
   )
 }
