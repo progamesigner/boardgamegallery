30806be41489d9c6bc804ee275785e3d464ab149
Yang Sheng Han <progamesigner@gmail.com>
2023-03-08 11:47:26 +0000

    Implement filtering

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,6 +1,7 @@
 import type { JSX, ParentProps } from 'solid-js'
 
-import { Match, Switch } from 'solid-js'
+import { createUniqueId, Match, Switch } from 'solid-js'
+import { Portal } from 'solid-js/web'
 
 export const enum TagColor {
   AMBER,
@@ -17,52 +18,170 @@ export const enum TagColor {
 }
 
 interface Props extends ParentProps {
+  active?: boolean
   color?: TagColor
+  onChange?: JSX.EventHandler<HTMLInputElement, Event>
 }
 
 export function Tag(props: Props): JSX.Element {
+  const key = createUniqueId()
+
   return (
     <Switch>
-      <Match when={props.color === TagColor.AMBER}>
-        <span class="badge rounded bg-amber-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.BLUE}>
-        <span class="badge rounded bg-blue-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.CYAN}>
-        <span class="badge rounded bg-cyan-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.GREEN}>
-        <span class="badge rounded bg-green-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.INDIGO}>
-        <span class="badge rounded bg-indigo-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.ORANGE}>
-        <span class="badge rounded bg-orange-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.PINK}>
-        <span class="badge rounded bg-pink-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.PURPLE}>
-        <span class="badge rounded bg-purple-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.RED}>
-        <span class="badge rounded bg-red-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.TEAL}>
-        <span class="badge rounded bg-teal-700 py-2">{props.children}</span>
-      </Match>
-      <Match when={props.color === TagColor.YELLOW}>
-        <span class="badge rounded bg-yellow-700 py-2">{props.children}</span>
+      <Match when={props.onChange}>
+        <Portal>
+          <input
+            id={key}
+            type="checkbox"
+            class="fixed h-0 w-0 appearance-none opacity-0"
+            onChange={event => props.onChange?.(event)}
+          />
+        </Portal>
+        <label for={key} class="cursor-pointer">
+          <Switch>
+            <Match when={props.color === TagColor.AMBER}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-amber-500': props.active, 'bg-amber-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.BLUE}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-blue-500': props.active, 'bg-blue-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.CYAN}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-cyan-500': props.active, 'bg-cyan-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.GREEN}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-green-500': props.active, 'bg-green-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.INDIGO}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-indigo-500': props.active, 'bg-indigo-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.ORANGE}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-orange-500': props.active, 'bg-orange-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.PINK}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-pink-500': props.active, 'bg-pink-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.PURPLE}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-purple-500': props.active, 'bg-purple-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.RED}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-red-500': props.active, 'bg-red-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.TEAL}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-teal-500': props.active, 'bg-teal-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={props.color === TagColor.YELLOW}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-yellow-500': props.active, 'bg-yellow-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+            <Match when={true}>
+              <span
+                class="badge rounded py-2"
+                classList={{ 'bg-gray-500': props.active, 'bg-gray-700': !props.active }}
+              >
+                {props.children}
+              </span>
+            </Match>
+          </Switch>
+        </label>
       </Match>
       <Match when={true}>
-        <span class="badge rounded bg-gray-700 py-2">{props.children}</span>
+        <Switch>
+          <Match when={props.color === TagColor.AMBER}>
+            <span class="badge rounded bg-amber-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.BLUE}>
+            <span class="badge rounded bg-blue-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.CYAN}>
+            <span class="badge rounded bg-cyan-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.GREEN}>
+            <span class="badge rounded bg-green-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.INDIGO}>
+            <span class="badge rounded bg-indigo-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.ORANGE}>
+            <span class="badge rounded bg-orange-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.PINK}>
+            <span class="badge rounded bg-pink-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.PURPLE}>
+            <span class="badge rounded bg-purple-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.RED}>
+            <span class="badge rounded bg-red-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.TEAL}>
+            <span class="badge rounded bg-teal-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={props.color === TagColor.YELLOW}>
+            <span class="badge rounded bg-yellow-700 py-2">{props.children}</span>
+          </Match>
+          <Match when={true}>
+            <span class="badge rounded bg-gray-700 py-2">{props.children}</span>
+          </Match>
+        </Switch>
       </Match>
     </Switch>
   )
 }
 
 export function Tags(props: ParentProps): JSX.Element {
-  return <div class="flex gap-2 justify-start flex-wrap">{props.children}</div>
+  return <div class="flex flex-wrap justify-start gap-2">{props.children}</div>
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
-import type { JSX } from 'solid-js'
+import type { JSX, Setter } from 'solid-js'
 
 import type { GameObject } from '~/types'
 
-import { createSignal, Index, Match, onMount, Show, Switch } from 'solid-js'
+import { createMemo, createSignal, Index, Match, onMount, Show, Switch } from 'solid-js'
 import { Portal } from 'solid-js/web'
 import { useSearchParams } from 'solid-start'
 
@@ -18,36 +18,155 @@ import { Tag, Tags } from '~/components/Tag'
 
 import { fetchGames } from '~/apis'
 
-function defaultPlayers(): Array<string> {
-  const players = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(players => `${players} 人`)
-  return [...players, '12 人以上']
+enum PlayerFilter {
+  SUPPORT_2 = 2,
+  SUPPORT_3 = 3,
+  SUPPORT_4 = 4,
+  SUPPORT_5 = 5,
+  SUPPORT_6 = 6,
+  SUPPORT_7 = 7,
+  SUPPORT_8 = 8,
+  SUPPORT_9 = 9,
+  SUPPORT_10 = 10,
+  SUPPORT_11 = 11,
+  SUPPORT_12 = 12,
+  SUPPORT_MORE_THAN_12 = 13,
 }
 
-function defaultTimes(): Array<string> {
-  const times = [15, 30, 45, 60, 90, 120, 150].map(players => `${players} 分鐘`)
-  return ['15 分鐘以內', ...times, '150 分鐘以上']
+enum TimeFilter {
+  SUPPORT_LESS_THAN_15 = 14,
+  SUPPORT_15 = 15,
+  SUPPORT_30 = 30,
+  SUPPORT_45 = 45,
+  SUPPORT_60 = 60,
+  SUPPORT_90 = 90,
+  SUPPORT_120 = 120,
+  SUPPORT_150 = 150,
+  SUPPORT_MORE_THAN_150 = 151,
 }
 
-function processTags(games: Array<GameObject>): Array<string> {
+type Filters = PlayerFilter | TimeFilter | string
+
+interface Filter<T> {
+  filter: T
+  value: string
+}
+
+function defaultPlayers(): Array<Filter<PlayerFilter>> {
+  return [
+    { filter: PlayerFilter.SUPPORT_2, value: '2 人' },
+    { filter: PlayerFilter.SUPPORT_3, value: '3 人' },
+    { filter: PlayerFilter.SUPPORT_4, value: '4 人' },
+    { filter: PlayerFilter.SUPPORT_5, value: '5 人' },
+    { filter: PlayerFilter.SUPPORT_6, value: '6 人' },
+    { filter: PlayerFilter.SUPPORT_7, value: '7 人' },
+    { filter: PlayerFilter.SUPPORT_8, value: '8 人' },
+    { filter: PlayerFilter.SUPPORT_9, value: '9 人' },
+    { filter: PlayerFilter.SUPPORT_10, value: '10 人' },
+    { filter: PlayerFilter.SUPPORT_11, value: '11 人' },
+    { filter: PlayerFilter.SUPPORT_12, value: '12 人' },
+    { filter: PlayerFilter.SUPPORT_MORE_THAN_12, value: '12 人以上' },
+  ]
+}
+
+function defaultTimes(): Array<Filter<TimeFilter>> {
+  return [
+    { filter: TimeFilter.SUPPORT_LESS_THAN_15, value: '15 分鐘以內' },
+    { filter: TimeFilter.SUPPORT_15, value: '15 分鐘' },
+    { filter: TimeFilter.SUPPORT_30, value: '30 分鐘' },
+    { filter: TimeFilter.SUPPORT_45, value: '45 分鐘' },
+    { filter: TimeFilter.SUPPORT_60, value: '60 分鐘' },
+    { filter: TimeFilter.SUPPORT_90, value: '90 分鐘' },
+    { filter: TimeFilter.SUPPORT_120, value: '120 分鐘' },
+    { filter: TimeFilter.SUPPORT_150, value: '150 分鐘' },
+    { filter: TimeFilter.SUPPORT_MORE_THAN_150, value: '150 分鐘以上' },
+  ]
+}
+
+function processTags(games: Array<GameObject>): Array<Filter<string>> {
   const tags = new Set<string>()
   games.forEach(game => {
     Array.prototype.concat([], game.types, game.tags).forEach(tag => tags.add(tag))
   })
-  return Array.from(tags)
+  return Array.from(tags).map(value => ({ filter: value, value: value }))
 }
 
 export default function (): JSX.Element {
   const [searchParams] = useSearchParams()
 
-  const [getError, setError] = createSignal<boolean>(false)
-  const [getGames, setGames] = createSignal<Array<GameObject>>([])
-  const [getTags, setTags] = createSignal<Array<string>>([])
-  const [getTimes] = createSignal<Array<string>>(defaultTimes())
-  const [getPlayers] = createSignal<Array<string>>(defaultPlayers())
-  const [getLoading, setLoading] = createSignal<boolean>(true)
   const [getSource, setSource] = createSignal<string | undefined>(
     import.meta.env.VITE_DEFAULT_STORE
   )
+
+  const [getLoading, setLoading] = createSignal<boolean>(true)
+  const [getError, setError] = createSignal<boolean>(false)
+
+  const [getFullGames, setFullGames] = createSignal<Array<GameObject>>([])
+
+  const [getTimes] = createSignal<Array<Filter<TimeFilter>>>(defaultTimes())
+  const [getPlayers] = createSignal<Array<Filter<PlayerFilter>>>(defaultPlayers())
+  const [getTags, setTags] = createSignal<Array<Filter<string>>>([])
+
+  const [getPlayerFilters, setPlayerFilters] = createSignal<Set<PlayerFilter>>(
+    new Set<PlayerFilter>()
+  )
+  const [getTimeFilters, setTimeFilters] = createSignal<Set<TimeFilter>>(new Set<TimeFilter>())
+  const [getTagFilters, setTagFilters] = createSignal<Set<string>>(new Set<string>())
+
+  const getGames = createMemo(() => {
+    const games = getFullGames()
+
+    const activePlayerFilters = Array.from(getPlayerFilters())
+    const activeTimeFilters = Array.from(getTimeFilters())
+    const activeTagFilters = Array.from(getTagFilters())
+
+    console.log(activePlayerFilters)
+    console.log(activeTimeFilters)
+    return games
+      .filter(
+        activePlayerFilters.length > 0
+          ? game =>
+              activePlayerFilters.filter(value => {
+                switch (value) {
+                  case PlayerFilter.SUPPORT_MORE_THAN_12:
+                    return game.maximalPlayers >= value
+                  default:
+                    return game.minimalPlayers <= value && value <= game.maximalPlayers
+                }
+              }).length > 0
+          : () => true
+      )
+      .filter(
+        activeTimeFilters.length > 0
+          ? game =>
+              activeTimeFilters.filter(value => {
+                switch (value) {
+                  case TimeFilter.SUPPORT_LESS_THAN_15:
+                    return game.minimalMinutes <= value
+                  case TimeFilter.SUPPORT_MORE_THAN_150:
+                    return game.maximalMinutes >= value
+                  default:
+                    return game.minimalMinutes <= value && value <= game.maximalMinutes
+                }
+              }).length > 0
+          : () => true
+      )
+      .filter(
+        activeTagFilters.length > 0
+          ? game =>
+              activeTagFilters.filter(value => [...game.types, ...game.tags].includes(value))
+                .length > 0
+          : () => true
+      )
+  })
+
+  const toggleFilter = <T extends Filters>(setter: Setter<Set<T>>, filter: T) => {
+    return () =>
+      setter(filters => {
+        const exists = filters.delete(filter)
+        return exists ? new Set([...filters.values()]) : new Set([...filters.values(), filter])
+      })
+  }
 
   if (
     import.meta.env.VITE_ENABLE_SOURCE_QUERY &&
@@ -61,7 +180,7 @@ export default function (): JSX.Element {
     if (source) {
       try {
         const games = await fetchGames(source)
-        setGames(games)
+        setFullGames(games)
         setTags(processTags(games))
         setError(false)
       } catch (error) {
@@ -79,10 +198,28 @@ export default function (): JSX.Element {
           <Header />
           <div class="flex flex-col gap-2 px-4">
             <Tags>
-              <Index each={getPlayers()}>{player => <Tag>{player()}</Tag>}</Index>
+              <Index each={getPlayers()}>
+                {player => (
+                  <Tag
+                    active={getPlayerFilters().has(player().filter)}
+                    onChange={toggleFilter(setPlayerFilters, player().filter)}
+                  >
+                    {player().value}
+                  </Tag>
+                )}
+              </Index>
             </Tags>
             <Tags>
-              <Index each={getTimes()}>{time => <Tag>{time()}</Tag>}</Index>
+              <Index each={getTimes()}>
+                {time => (
+                  <Tag
+                    active={getTimeFilters().has(time().filter)}
+                    onChange={toggleFilter(setTimeFilters, time().filter)}
+                  >
+                    {time().value}
+                  </Tag>
+                )}
+              </Index>
             </Tags>
             <Switch>
               <Match when={getLoading()}>
@@ -93,7 +230,16 @@ export default function (): JSX.Element {
               <Match when={true}>
                 <Tags>
                   <Show when={getTags().length > 0}>
-                    <Index each={getTags()}>{tag => <Tag>{tag()}</Tag>}</Index>
+                    <Index each={getTags()}>
+                      {tag => (
+                        <Tag
+                          active={getTagFilters().has(tag().filter)}
+                          onChange={toggleFilter(setTagFilters, tag().filter)}
+                        >
+                          {tag().value}
+                        </Tag>
+                      )}
+                    </Index>
                   </Show>
                 </Tags>
               </Match>
