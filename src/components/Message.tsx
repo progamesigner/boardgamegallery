e33a1d16f9b5739eb869796566f9a6bb4d6070e0
Yang Sheng Han <progamesigner@gmail.com>
2023-03-08 14:07:40 +0000

    Fix hydration warning

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,16 +1,10 @@
 import type { JSX, ParentProps } from 'solid-js'
 
-import { Show } from 'solid-js'
-
 export function Message(props: ParentProps): JSX.Element {
   return (
     <div class="flex h-96 max-h-screen justify-center">
       <div class="card">
-        <div class="card-body flex max-h-screen flex-row items-center">
-          <Show when={props.children} fallback={<p>發生未知錯誤⋯⋯</p>}>
-            {props.children}
-          </Show>
-        </div>
+        <div class="card-body flex max-h-screen flex-row items-center">{props.children}</div>
       </div>
     </div>
   )
